ebb3e46bc27fbdb459ee39cfc084d8df7aec9d68
Gurnoor Singh Pannu <gurnxxrpannu@gmail.com>
2025-09-01 22:34:16 +0530

    removed unused imports from home screen

--- app/src/main/java/com/example/breedify/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/breedify/screens/homeScreen/HomeScreen.kt
@@ -11,21 +11,16 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.layout.ContentScale
-
-import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import androidx.compose.ui.unit.dp
